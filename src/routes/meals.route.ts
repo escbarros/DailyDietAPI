69d354dfde0a683070bcc7b87e1c6e9bfd9fe7f4
Eduardo Barros <eduardoscbarros@hotmail.com>
2023-07-03 00:49:38 -0300

    added post meal

--- src/routes/meals.route.ts
+++ src/routes/meals.route.ts
@@ -1,6 +1,36 @@
+import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { knex } from "../database";
 export async function MealsRoute(app: FastifyInstance) {
-  app.get("/", (req, res) => {
+  app.get("/", async (req, res) => {
     return res.status(200).send("teste");
+  });
+
+  app.post("/", async (req, res) => {
+    const createMealRequisitionSchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isInDiet: z.boolean(),
+    });
+    const { name, description, isInDiet } = createMealRequisitionSchema.parse(
+      req.body
+    );
+
+    let sessionId = req.cookies.sessionId;
+    if (!sessionId) {
+      sessionId = randomUUID();
+      res.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days,
+      });
+    }
+
+    await knex("meal").insert({
+      name,
+      description,
+      session_id: sessionId,
+      isInDiet,
+    });
   });
 }
